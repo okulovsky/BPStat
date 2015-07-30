0cecf0b075b26d8b18d89d5b016766a21f517dae
okulovsky <yuri.okulovsky@gmail.com>
2015-07-30 08:43:24 +0500

    some queries

--- Statistics/Classes.cs
+++ Statistics/Classes.cs
@@ -1,13 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Statistics
 {
-	public class Visit
+    public interface IReadable
+    {
+        void Load(string[] data);
+    }
+	public class Visit : IReadable
 	{
+        public string SlideId { get; set; }
+        public DateTime TimeStamp { get; set;}
+        public int Score { get; set; }
+        public int AttemptsCount { get; set; }
+        public bool IsSkiped { get; set; }
+        public bool IsPassed { get; set; }
+        public string UserId { get; set; }
 
+        public Slide SlideInfo { get; set; }
+
+        public void Load(string[] data)
+        {
+            SlideId = data[0];
+            TimeStamp = DateTime.Parse(data[1]);
+            Score = int.Parse(data[2]);
+            AttemptsCount = int.Parse(data[3]);
+            IsSkiped = data[4] != "0";
+            IsPassed = data[5] != "0";
+            UserId = data[6];
+        }
 	}
+
+    public class UserSolutions : IReadable
+    {
+        public string SlideId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool IsRightAnswer { get; set; }
+        public bool IsCompilationError { get; set; }
+        public string UserId { get; set; }
+
+        public void Load(string[] data)
+        {
+            SlideId = data[0];
+            TimeStamp = DateTime.Parse(data[1]);
+            IsRightAnswer = data[2] != "0";
+            IsCompilationError = data[3] != "0";
+            UserId = data[4];
+        }
+    }
+
+    public class Slide : IReadable
+    {
+        public int Number { get; set; }
+        public string Id { get; set; }
+        public string Caption { get; set; }
+        public string Unit { get; set; }
+
+        public void Load(string[] data)
+        {
+            Number = int.Parse(data[0]);
+            Id = data[1];
+            Caption = data[2];
+            Unit = data[3];
+        }
+
+
+    }
+
+    public class Loader
+    {
+
+
+        public static T[] Load<T>(string path)
+            where T : IReadable,new()
+        {
+          
+            return File.
+                ReadLines(path,System.Text.Encoding.Default).
+                Skip(1).
+                Select(line => line.Split(';')).
+                Select(z => { var r = new T(); r.Load(z); return r; })
+                .ToArray();
+        }
+    }
 }
--- Statistics/Program.cs
+++ Statistics/Program.cs
@@ -3,18 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Statistics
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			
-		}
-	}
+    static class LinqExtensions
+    {
+        public static void ShowHisto<T>(this IEnumerable<T> data, Func<T, int> x, Func<T, int> y)
+        {
+            var chart = new Chart();
+            chart.ChartAreas.Add(new ChartArea());
+
+            var serie = new Series();
+
+            foreach (var e in data)
+                serie.Points.Add(new DataPoint(x(e), y(e)));
+
+            serie.ChartType = SeriesChartType.Bar;
+            chart.Series.Add(serie);
+            chart.Dock = DockStyle.Fill;
+            var form = new Form();
+            form.Controls.Add(chart);
+            Application.Run(form);
+        }
+
+        public static T ArgMax<T>(this IEnumerable<T> data, Func<T,int> selector)
+        {
+            return data.ArgMin(x=>-selector(x));
+        }
+
+        public static T ArgMin<T>(this IEnumerable<T> data, Func<T, int> selector)
+        {
+            T result = default(T);
+            var measure = -1;
+            bool first = true;
+            foreach (var e in data)
+            {
+                if (first)
+                {
+                    result = e;
+                    measure = selector(e);
+                    first = false;
+                    continue;
+                }
+                var tmeasure = selector(e);
+                if (tmeasure < measure)
+                {
+                    result = e;
+                    measure = tmeasure;
+                }
+            }
+            if (first) throw new Exception("No elements");
+            return result;
+        }
+    }
+
+    static class Program
+    {
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var slides = Loader.Load<Slide>(@"..\..\..\slides.csv");
+           
+            var visits = Loader.Load<Visit>(@"..\..\..\visits.csv");
+           var solutions = Loader.Load<UserSolutions>(@"..\..\..\usersolutions.csv");
+           
+            foreach(var e in visits)
+                e.SlideInfo = slides.Where(z=>z.Id==e.SlideId).FirstOrDefault();
+
+            visits=visits.Where(z=>z.SlideInfo!=null).ToArray();
+
+            //visits
+            //    .GroupBy(z => z.UserId)
+            //    .Select(z => new { UserId = z.Key, Count = z.Count()})
+            //    .GroupBy(z => z.Count/10)
+            //    .Select(z => new { CountOfTasks = 10*z.Key, CountOfUsers = z.Count() })
+            //    .OrderBy(z=>z.CountOfTasks)
+            //    .Skip(3)
+            //    .ShowHisto(z => z.CountOfTasks, z => z.CountOfUsers);
+
+            var lastSlides =
+                visits
+                .GroupBy(z => z.UserId)
+                .Select(z => new { UserId = z.Key, Last = z.ArgMax(x => x.SlideInfo.Number) })
+                .GroupBy(z => z.Last.SlideInfo.Number)
+                .Select(z => new { LastTask = z.Key, CountOfUsers = z.Count() })
+                .ToArray();
+
+            lastSlides
+                .OrderBy(z => z.LastTask)
+                .ShowHisto(z => z.LastTask, z => z.CountOfUsers);
+
+            //var hardProblems =
+            //    lastSlides
+            //    .OrderByDescending(z => z.CountOfUsers)
+            //    .Take(20)
+            //    .Select(z => z.LastTask.SlideInfo.Caption)
+            //    .ToArray();
+        }
+    }
 }
