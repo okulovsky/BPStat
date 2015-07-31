7400ad60463fa5231520783b5e83f8e52f6de803
okulovsky <yuri.okulovsky@gmail.com>
2015-07-31 16:33:06 +0500

    + first readable results

---
+++ Statistics/LinqAdditions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Statistics
+{
+	static class LinqExtensions
+	{
+		public static void ShowHisto<T>(this IEnumerable<T> data, Func<T, int> x, Func<T, int> y)
+		{
+			var chart = new Chart();
+			chart.ChartAreas.Add(new ChartArea());
+
+			var serie = new Series();
+
+			foreach (var e in data)
+				serie.Points.Add(new DataPoint(x(e), y(e)));
+
+			serie.ChartType = SeriesChartType.Bar;
+			chart.Series.Add(serie);
+			chart.Dock = DockStyle.Fill;
+			var form = new Form();
+			form.Controls.Add(chart);
+			Application.Run(form);
+		}
+
+		public static void ForEach<T>(this IEnumerable<T> data, Action<T> action)
+		{
+			foreach (var e in data) action(e);
+		}
+
+		public static void ShowDistribution<T>(this IEnumerable<T> data, Func<T, double> value, Func<T, bool> filter, int compression)
+		{
+			data
+				.Where(filter)
+				.GroupBy(z => (int)(value(z) / compression))
+				.ShowHisto(z => z.Key * compression, z => z.Count());
+		}
+
+		public static Tuple<IEnumerable<T>, Func<object, string>[]> Prepare<T>(this IEnumerable<T> data, params string[] captions)
+		{
+			var list = new List<Func<object, string>>();
+			foreach (var e in captions)
+			{
+				Console.Write("{0} ", e);
+				list.Add(new Func<object, string>(o =>
+				{
+					var s = o.ToString();
+					if (s.Length > e.Length) s = s.Substring(0, e.Length);
+					var fmt = ("{0," + e.Length + "}");
+					s = string.Format(fmt, s);
+					return s;
+				}));
+
+			}
+			Console.WriteLine();
+			return Tuple.Create(data, list.ToArray());
+		}
+
+		public static void Output<T>(this Tuple<IEnumerable<T>, Func<object, string>[]> prep, bool skip, params Func<T, object>[] selectors)
+		{
+			if (skip) return;
+			var data = prep.Item1;
+			var fms = prep.Item2;
+			if (fms.Length != selectors.Length) throw new ArgumentException();
+
+
+			foreach (var e in data)
+			{
+				for (int i = 0; i < selectors.Length; i++)
+				{
+					var s = fms[i](selectors[i](e));
+					Console.Write(s + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+
+		public static T ArgMax<T>(this IEnumerable<T> data, Func<T, int> selector)
+		{
+			return data.ArgMin(x => -selector(x));
+		}
+
+		public static T ArgMin<T>(this IEnumerable<T> data, Func<T, int> selector)
+		{
+			T result = default(T);
+			var measure = -1;
+			bool first = true;
+			foreach (var e in data)
+			{
+				if (first)
+				{
+					result = e;
+					measure = selector(e);
+					first = false;
+					continue;
+				}
+				var tmeasure = selector(e);
+				if (tmeasure < measure)
+				{
+					result = e;
+					measure = tmeasure;
+				}
+			}
+			if (first) throw new Exception("No elements");
+			return result;
+		}
+	}
+}
--- Statistics/Program.cs
+++ Statistics/Program.cs
@@ -7,113 +7,79 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Statistics
 {
-	static class LinqExtensions
-	{
-		public static void ShowHisto<T>(this IEnumerable<T> data, Func<T, int> x, Func<T, int> y)
-		{
-			var chart = new Chart();
-			chart.ChartAreas.Add(new ChartArea());
-
-			var serie = new Series();
-
-			foreach (var e in data)
-				serie.Points.Add(new DataPoint(x(e), y(e)));
-
-			serie.ChartType = SeriesChartType.Bar;
-			chart.Series.Add(serie);
-			chart.Dock = DockStyle.Fill;
-			var form = new Form();
-			form.Controls.Add(chart);
-			Application.Run(form);
-		}
-
-		public static void ShowDistribution<T>(this IEnumerable<T> data, Func<T, double> value, Func<T, bool> filter, int compression)
-		{
-			data
-				.Where(filter)
-				.GroupBy(z => (int)(value(z) / compression))
-				.ShowHisto(z => z.Key * compression, z => z.Count());
-		}
-
-		public static Tuple<IEnumerable<T>,Func<object,string>[]> Prepare<T>(this IEnumerable<T> data, params string[] captions)
-		{
-			var list=new List<Func<object,string>>();
-			foreach(var e in captions)
-			{
-				Console.Write("{0} ",e);
-				list.Add(new Func<object,string>(o=>
-					{
-						var s = o.ToString();
-						if (s.Length>e.Length) s=s.Substring(0,e.Length);
-						var fmt=("{0,"+e.Length+"}");
-						s=string.Format(fmt,s);
-						return s;
-					}));
-				
-			}
-			Console.WriteLine();
-			return Tuple.Create(data, list.ToArray());
-		}
 	
-		public static void Output<T>(this Tuple<IEnumerable<T>,Func<object,string>[]> prep, bool skip, params Func<T,object>[] selectors)
-		{
-			if (skip) return;
-			var data = prep.Item1;
-			var fms = prep.Item2;
-			if (fms.Length != selectors.Length) throw new ArgumentException();
-			
-
-			foreach(var e in data)
-			{
-				for (int i = 0; i < selectors.Length; i++)
-				{
-					var s = fms[i](selectors[i](e));
-					Console.Write(s + " ");
-				}
-				Console.WriteLine();
-			}
-		}
-
-
-		public static T ArgMax<T>(this IEnumerable<T> data, Func<T, int> selector)
-		{
-			return data.ArgMin(x => -selector(x));
-		}
-
-		public static T ArgMin<T>(this IEnumerable<T> data, Func<T, int> selector)
-		{
-			T result = default(T);
-			var measure = -1;
-			bool first = true;
-			foreach (var e in data)
-			{
-				if (first)
-				{
-					result = e;
-					measure = selector(e);
-					first = false;
-					continue;
-				}
-				var tmeasure = selector(e);
-				if (tmeasure < measure)
-				{
-					result = e;
-					measure = tmeasure;
-				}
-			}
-			if (first) throw new Exception("No elements");
-			return result;
-		}
-	}
 
 	static class Program
 	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Load();
+			PrepareUsers();
+
+			//В выборке до фига мусорных пользователей, которые курс до конца не то что не досмотрели, но даже не начали.
+			//Диаграмма показывает количество пользователей в зависимости от просмотренных ими лекций
+			//userData.ShowDistribution(z => z.Value.ViewedLecturesCount, z=>true, 10);
+			//нас будут интересовать только пользователи, которые посмотрели хотя бы 50 лекций
+
+
+			//Распределение пользователей по запаздыванию захода на слайд
+			//Большая часть пользователь смотрела слайды сразу же после их опубликование. Имеющееся смещение объясняется, вероятно, 
+			//просмотром при подготовке к экзамену
+			//У нас также есть странные люди, которые запаздывали на 200 и более дней. Это даже не экзамен, это самостоятельное изучение.
+			//Вероятно, их нужды нужно изучать отдельно.
+			//userData.ShowDistribution(z => z.Value.LecturesViewsLate, z => true, 7);
+
+			//Мы будем учитывать только тех людей, которые посмотрели достаточно много лекций с не слишком большим опозданием
+			foreach (var e in userData)
+				e.Value.GoodUser = e.Value.LecturesViewsLate < 40 && e.Value.ViewedLecturesCount > 50;
+
+			var goodUsersCount = userData.Count(z => z.Value.GoodUser);
+			//Их 160 человек
+
+			var peopleDoNotSolveExercises = userData.Count(z => z.Value.GoodUser && z.Value.LastExercise == null);
+			//Из них только 1 человек не сделал ни одного упражнения
+
+			//Вот сколько они решают задач
+			//userData.ShowDistribution(z => z.Value.SolvedExerciseCount, z => z.Value.GoodUser, 5);
+			//Я вижу тут нормальное распределение в центре с 25 + упорные, которые доходят до конца. 
+			//Учитывая, что у нас всего в 13 лекциях есть задачи, оптимумом является решение по 2 задачи в лекцию.
+
+			PrepareExercises();
+
+			//Распределение количества задач по количеству попыток. Видно, что задача занимает от 1 до 15 попыток.
+			//exercisesData.ShowDistribution(z => z.Value.AverageAttempts, z => true, 1);
+
+			//График показывает, какие пользователи когда бросили решать задачи
+			//userData.ShowDistribution(z => z.Value.LastExercise.UnitData.Number, z => z.Value.GoodUser && z.Value.LastExercise != null , 1);
+			//В основном, они бросили на последней лекции (что логично). Остальные в целом нормальным образом сидят между 0 и 10 лекцией. 
+			//Учитывая то, что распределение действительно выглядит нормальным, я бы не стал связывать факт бросания человеком задачи с ее сложностью: 
+			//решение о бросании задачи есть случайный процесс.
+
+			//Собирательная статистика по задачам
+			exercisesData
+				.OrderByDescending(z=>z.Value.Fails)
+				.Prepare("Заголовок         ", "Unit", "Fail", "Attem", "Pop", "Brkdwn")
+				.Output(false,
+				z => z.Key.Caption,
+				z=>z.Key.UnitData.Number,
+				z => z.Value.Fails,
+				z => z.Value.AverageAttempts,
+				z => z.Value.Popularity,
+				z => z.Value.CountOfBreakdownTimes);
+
+		}
+
+		#region Чтение данных из файла
+
 		static Visit[] visits;
 		static Unit[] units;
 		static UserSolutions[] solutions;
 		static Slide[] slides;
 		static DateTime beginning;
-
 
 
 		static void Clean<T>(ref T[] array, Func<T, bool> filter)
@@ -150,32 +116,20 @@ namespace Statistics
 			Clean(ref visits, z => z.SlideInfo != null);
 			Clean(ref solutions, z => z.SlideInfo != null);
 		}
+		#endregion
+		#region Промежуточный обсчет данных
 
-
-		static Dictionary<string, double> UserDistribution<T>(bool show, IEnumerable<T> array, Func<T, bool> filter, Func<T, string> userSelector, Func<IEnumerable<T>, double> agregator, int compression)
-		{
-			var data = array
-			  .Where(filter)
-			  .GroupBy(userSelector)
-			  .Select(z => new { UserId = z.Key, Value = agregator(z) })
-			  .OrderBy(z => z.Value)
-			  .ToDictionary(z => z.UserId, z => z.Value);
-
-			if (show)
-			{
-				data
-				  .GroupBy(z => (int)(z.Value / compression))
-				  .ShowHisto(z => z.Key * compression, z => z.Count());
-			}
-
-			return data;
-		}
+		static Dictionary<string, UserVisitingData> userData;
+		static Dictionary<Slide, ExerciseData> exercisesData;
 
 		class UserVisitingData
 		{
 			public int ViewedLecturesCount { get; set; }
 			public double LecturesViewsLate { get; set; }
 			public bool GoodUser { get; set; }
+			public Slide LastExercise { get; set; }
+			public int SolvedExerciseCount { get; set; }
+			public int NonSolvedExerciseCount { get; set; }
 		}
 
 		static UserVisitingData ProcessUserVisits(IEnumerable<Visit> visits)
@@ -187,110 +141,72 @@ namespace Statistics
 		}
 
 
-		class UserSolutionsData
+		static void ProcessUserSolutions(IEnumerable<UserSolutions> solutions, UserVisitingData data)
 		{
-			public Slide LastExercise { get; set; }
-		}
+			data.SolvedExerciseCount = solutions.GroupBy(z => z.SlideId).Select(z => z.Any(x => x.IsRightAnswer)).Count();
+			data.NonSolvedExerciseCount = solutions.GroupBy(z => z.SlideId).Select(z => z.All(x => !x.IsRightAnswer)).Count();
 
-		static UserSolutionsData ProcessUserSolutions(IEnumerable<UserSolutions> solutions)
-		{
-			var data = new UserSolutionsData();
 			data.LastExercise = solutions.Where(z => z.SlideInfo.Type == SlideType.Exercise).ArgMax(z => z.SlideInfo.Number).SlideInfo;
-			return data;
 		}
 
 		class ExerciseData
 		{
-			public double FailurePercentage { get; set; }
+			public double Fails { get; set; }
 			public double AverageAttempts { get; set; }
 			public double MaxAttempts { get; set; }
 			public double Popularity { get; set; }
 			public int CountOfBreakdownTimes { get; set; }
+
 		}
 
-		static ExerciseData ProcessExerciseAttempts(this IEnumerable<Visit> v)
+		static ExerciseData ProcessExerciseVisit(this IEnumerable<Visit> v)
 		{
 			var data = new ExerciseData();
-			data.Popularity = v.Count();
-			data.FailurePercentage = (double)v.Count(z=>!z.IsPassed)/data.Popularity;
 			data.MaxAttempts = v.Max(z => z.AttemptsCount);
 			data.AverageAttempts = v.Average(z => z.AttemptsCount);
 			return data;
 		}
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-        {
-            Load();
 
-            /*
-            //Исследуем, как по времени пользователи пользуются системой.
+		static void ProcessExerciseSolution(IEnumerable<UserSolutions> v, ExerciseData data)
+		{
+			data.Popularity = v.Select(z=>z.UserId).Distinct().Count();
+			data.Fails = v.GroupBy(z => z.UserId).Where(z => z.All(x => !x.IsRightAnswer)).Count();
+		}
 
-            //количество заходов на слайд в зависимости от времени, прошедшего с начала семестра
-            visits
-                .GroupBy(z => (int)((z.TimeStamp - beginning).TotalDays)/7)
-                .Where(z=>z.Key>0)
-                .ShowHisto(z => z.Key, z => z.Count());
-
-
-            //Количество заходов на слайд в зависимости от времени, прошедшего с момента его публикации
-            visits
-                .GroupBy(z => (int)((z.TimeStamp - z.SlideInfo.UnitData.PublishingData).TotalDays)/7)
-                .Where(z=>z.Key>0)
-                .ShowHisto(z => z.Key, z => z.Count());
-            */
-			var visiting = visits
+		
+		static void PrepareUsers()
+		{
+			userData = visits
 			  .GroupBy(v => v.UserId)
 			  .ToDictionary(z => z.Key, ProcessUserVisits);
-             
-            //В выборке до фига мусорных пользователей, которые курс до конца не то что не досмотрели, но даже не начали.
-			//visiting.ShowDistribution(z => z.Value.ViewedLecturesCount, z=>true, 10);
-            //нас будут интересовать только пользователи, которые посмотрели хотя бы 50 лекций
-            
-			
-			//Распределение пользователей по запаздыванию захода на слайд
-           // visiting.ShowDistribution(z=>z.Value.LecturesViewsLate,z=>true,7);
 
-			foreach (var e in visiting)
-				e.Value.GoodUser = e.Value.LecturesViewsLate < 40 && e.Value.ViewedLecturesCount > 50;
+			solutions
+			 .Where(v => v.UserId != "NULL")
+			 .GroupBy(v => v.UserId)
+			 .ForEach(z => ProcessUserSolutions(z, userData[z.Key]));
+		}
 
-			var solving = solutions
-			  .Where(v=>v.UserId!="NULL")
-			  .GroupBy(v => v.UserId)
-			  .ToDictionary(z => z.Key, ProcessUserSolutions);
-
-			//solving.ShowDistribution(z => z.Value.LastExercise.UnitData.Number,z=>visiting[z.Key].GoodUser, 1);
-
-			//visits.ShowDistribution(z => z.AttemptsCount, z => visiting[z.UserId].GoodUser && z.AttemptsCount>20, 1);
-
-			//visits.ShowDistribution(z => z.IsPassed ? 2 : (z.IsSkiped ? 1 : 0), z => visiting[z.UserId].GoodUser,1);
-
-			var lastProblems = solving
-				.Where(z=>visiting[z.Key].GoodUser)
-				.GroupBy(z=>z.Value.LastExercise)
-				.Select(z=>new { Exercise = z.Key, CountOfUsersThatBrokeOnIt = z.Count()})
-				.OrderByDescending(z=>z.CountOfUsersThatBrokeOnIt)
-				.ToArray();
-
-			var exercises = visits
-				.Where(z => visiting[z.UserId].GoodUser && z.SlideInfo.Type == SlideType.Exercise)
+		static void PrepareExercises()
+		{
+			exercisesData = visits
+				.Where(z => userData[z.UserId].GoodUser && z.SlideInfo.Type == SlideType.Exercise)
 				.GroupBy(z => z.SlideInfo)
-				.ToDictionary(z=>z.Key, ProcessExerciseAttempts);
+				.ToDictionary(z => z.Key, ProcessExerciseVisit);
 
-			foreach (var e in exercises)
-				e.Value.CountOfBreakdownTimes = solving.Count(z => z.Value.LastExercise.Id == e.Key.Id);
+			solutions
+				.Where(z => z.UserId != "NULL")
+				.Where(z => userData[z.UserId].GoodUser)
+				.GroupBy(z => z.SlideInfo)
+				.ForEach(z => ProcessExerciseSolution(z, exercisesData[z.Key]));
 
-			exercises
-				.Prepare("Заголовок         ","Fail%", "Attem", "Pop", "Brkdwn")
-				.Output(false, 
-				z => z.Key.Caption,
-				z => z.Value.FailurePercentage,
-				z => z.Value.AverageAttempts,
-				z => z.Value.Popularity,
-				z => z.Value.CountOfBreakdownTimes);
-			
-       }
+			foreach (var e in exercisesData)
+				e.Value.CountOfBreakdownTimes = userData.Count(z => z.Value.GoodUser && z.Value.LastExercise != null && z.Value.LastExercise.Id == e.Key.Id);
+		}
+
+		#endregion
+		
 	}
+
+
+
 }
